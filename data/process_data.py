dd566bc55e02200acc5f1dfa089c414808644780
Nasser Alsubait <nni141@gmail.com>
2019-08-15 05:37:56 +0300

    The script of ETL pipeline is ready

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,41 @@
 import sys
-
+import pandas as pd
+import numpy as np
+import sqlite3
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages,categories,on='id')
+    
+    return df
+
+    
+
 
 
 def clean_data(df):
-    pass
+    categories = df['categories'].str.split(';',expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.to_list()
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x.split('-')[1])
+    
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(lambda x: int(x))
+    
+    df.drop('categories',axis=1,inplace=True)
+    df = pd.concat([df,categories],axis=1)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    conn = sqlite3.connect(database_filename)  
+    df.to_sql('messages',con = conn, if_exists='replace',index=False)
 
 
 def main():
